08929bf68f0180723b6db0d1ccd94c7ac9394dd3
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-10-17 22:56:18 +0200

    Scaffold a template CRA site

---
+++ fe/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>
+        <img alt="logo" src="logo.png" />
+        Satier
+      </h1>
+    </>
+  );
+}
+
+export default App;
